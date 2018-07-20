ea877c1b93cdfe4a190c8d86c97ab271f677e435
Mohit Kishore <gkmohit@gmail.com>
2018-07-20 16:47:01 -0400

    Fixed issue with none

--- src/components/Book.js
+++ src/components/Book.js
@@ -8,6 +8,8 @@ class Book extends Component {
         changeShelf: PropTypes.func
     }
 
+    
+
     render() {
         
         let thumbnail;
@@ -17,23 +19,25 @@ class Book extends Component {
             thumbnail = this.props.book.imageLinks.thumbnail
         }
 
+        const style = {
+            width: 128, 
+            height: 193, 
+            backgroundImage: `url(${
+                thumbnail
+            })`};
+
         return(
             <div className="book">
                 <div className="book-top">
                     <div 
                         className="book-cover" 
-                        style={{ 
-                            width: 128, 
-                            height: 193, 
-                            backgroundImage: `url(${
-                                thumbnail
-                            })`}}>                        
+                        style={style}>                        
                     </div>
                     <div className="book-shelf-changer">
                         <select 
                             value={this.props.book.shelf ? this.props.book.shelf : "none"}
                             onChange={event => this.props.changeShelf(this.props.book, event.target.value)}>
-                                <option value="none" disabled>Move to...</option>
+                                <option value="moveTo" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
                                 <option value="read">Read</option>
